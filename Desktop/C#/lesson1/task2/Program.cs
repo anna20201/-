0f28761ee49387855fc657d2f029a6e69e3636eb
Анна <87242449+anna20201@users.noreply.github.com>
2024-04-26 14:18:11 +0300

    создали массив

--- Desktop/C#/lesson1/task2/Program.cs
+++ Desktop/C#/lesson1/task2/Program.cs
@@ -1,25 +1,14 @@
-﻿int i1=5;
-int i2=6;
-int sum=i1+i2;
-Console.WriteLine(sum);
+﻿//int[] arr = new int[5];
+//arr[0] = 5;
+//arr[1] = 3;
+//arr[2] = 2;
+//arr[3] = 4;
+//arr[4] = 7;
+//Console.Write(arr[0]);
+//Console.Write(arr[1]);
+//Console.Write(arr[2]);
+//Console.Write(arr[3]);
+//Console.Write(arr[4]);
 
-long l1=6;
-long sum2=l1+i1;
-Console.WriteLine(sum2);
-
-double d1=5.5;
-double sum3=d1+i1;
-
-int product1=i1*i2;
-long product2=l1*i1;
-double product3=i1*d1;
-Console.WriteLine(product1);
-Console.WriteLine(product2);
-Console.WriteLine(product3);
-
-int quotient=i1/i2;
-Console.WriteLine(quotient);
-
-d1=6;
-double quotient2=i1/d1;
-Console.WriteLine(quotient2);
+int[] arr2 = new int[5]{1, 2, 3, 4, 5};
+Console.Write(arr2[4]);
